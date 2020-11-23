a963a1b52632f86dc22a361232badb9405e5a71d
Solvence <anthonyhmu@gmail.com>
2020-11-22 20:29:17 -0500

    interactive view complete

--- animator/src/cs3500/animator/view/InteractiveAnimatorView.java
+++ animator/src/cs3500/animator/view/InteractiveAnimatorView.java
@@ -1,16 +1,59 @@
 package cs3500.animator.view;
 
+import cs3500.animator.model.dimension2d.Dimension2D;
+import cs3500.animator.model.position2d.Position2D;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 /**
- * A AnimatorView that is capable of telling if user input has been preformed.
+ * Represents an interactive animator view implementation that has a number of buttons to support
+ * functionality such as Pausing, speeding up, slowing down, and resetting the animation.
  */
-public interface InteractiveAnimatorView extends ActiveAnimatorView{
+public class InteractiveAnimatorView extends VisualAnimatorView implements ActionableAnimatorView {
+  private JButton pauseButton, speedUpButton, slowDownButton, resetButton;
+  private JPanel buttonPanel;
 
   /**
-   * Adds a Listener to a component, so we may know when an action has been preformed on it.
-   * @param listener Object that expects an action, and processes the result of the action.
+   * Constructs an interactive animator view given the canvas arguments.
+   * @param canvasPos - the top left position of the viewable canvas
+   * @param canvasSize - the width and height of the viewable canvas
    */
-  void setListener(ActionListener listener);
+  public InteractiveAnimatorView(Position2D canvasPos, Dimension2D canvasSize) {
+    super(canvasPos, canvasSize);
 
+    //button panel
+    buttonPanel = new JPanel();
+    buttonPanel.setLayout(new FlowLayout());
+    this.add(buttonPanel, BorderLayout.SOUTH);
+
+    //buttons
+    pauseButton = new JButton("Pause");
+    pauseButton.setActionCommand("Pause Button");
+    buttonPanel.add(pauseButton);
+
+    speedUpButton = new JButton("Speed Up");
+    speedUpButton.setActionCommand("Speed Up Button");
+    buttonPanel.add(speedUpButton);
+
+    slowDownButton = new JButton("Slow Down");
+    slowDownButton.setActionCommand("Slow Down Button");
+    buttonPanel.add(slowDownButton);
+
+    resetButton = new JButton("Reset");
+    resetButton.setActionCommand("Reset Button");
+    buttonPanel.add(resetButton);
+
+    this.pack();
+  }
+
+  @Override
+  public void setListener(ActionListener listener) {
+    pauseButton.addActionListener(listener);
+    speedUpButton.addActionListener(listener);
+    slowDownButton.addActionListener(listener);
+    resetButton.addActionListener(listener);
+  }
 }
